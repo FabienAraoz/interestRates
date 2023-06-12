51cf11518fed5505df7037a247e0d7090ee7790e
Gonzalo Araoz <FabienAraoz@outlook.com>
2023-06-11 23:45:18 -0500

    Proyecto en 0, completamente nuevo

---
+++ src/main/java/pe/io/fabien/Servlet.java
@@ -0,0 +1,10 @@
+package pe.io.fabien;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/procesador")
+public class Servlet extends HttpServlet{
+    //Sobreescribimos el método doPost para esta vez enviar inforamción al Servlet;
+    
+}
