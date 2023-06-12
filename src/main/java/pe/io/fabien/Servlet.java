0d31d206e572a39a9f86851e17530ef262500b31
Gonzalo Araoz <FabienAraoz@outlook.com>
2023-06-12 00:36:48 -0500

    Creamos una ruta y su Clas Formulas.java, para el código de ejecución

--- src/main/java/pe/io/fabien/Servlet.java
+++ src/main/java/pe/io/fabien/Servlet.java
@@ -1,10 +1,17 @@
 package pe.io.fabien;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/procesador")
 public class Servlet extends HttpServlet{
-    //Sobreescribimos el método doPost para esta vez enviar inforamción al Servlet;
-    
+    //Sobreescribimos el método doPost(): porque segun la IA es excelente para responder.
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        String casilla1 = request.getParameter("unidad_1");
+        String casilla2 = request.getParameter("unidad_2");
+        String casilla3 = request.getParameter("unidad_3");
+        
+        //Vamos a procesar los datos con el código extraido del siguiente formulario:
+        
+    }
 }
---
+++ src/main/java/pe/io/running/Formulas.java
@@ -0,0 +1,5 @@
+package pe.io.running; 
+
+public class Formulas{
+    
+}
