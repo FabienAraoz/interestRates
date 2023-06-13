515799ce21ad2a01eb6041dab07595060258f0f2
Gonzalo Araoz <FabienAraoz@outlook.com>
2023-06-13 00:15:00 -0500

    Primer Test.1 -Enviamos la respuesta al cliente, y creamos un form en el html

--- src/main/java/pe/io/fabien/Servlet.java
+++ src/main/java/pe/io/fabien/Servlet.java
@@ -1,24 +1,39 @@
 package pe.io.fabien;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import pe.io.running.Formulas;
 import pe.io.servicio.Calculadora;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/procesador")
-public class Servlet extends HttpServlet{
+public class Servlet extends HttpServlet {
+
     //Sobreescribimos el método doPost(): porque segun la IA es excelente para responder.
-    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         //Ceramos un Objeto prueba:
         Formulas objetoUsuario = new Formulas();
-        
+
         objetoUsuario.setCapital(Integer.parseInt(request.getParameter("userCapital")));
         objetoUsuario.setTasa(Integer.parseInt(request.getParameter("userInterest")));
         objetoUsuario.setnPeriodos(Integer.parseInt(request.getParameter("userTimes")));
-        
+
         //Damos a ejecutar el método de interes Simple:
         Calculadora test = new Calculadora();
-        test.interes_simple(objetoUsuario);
-        
+        int resultado;
+        resultado =test.interes_simple(objetoUsuario);
+
+        //Reenviamos el formulario a un nuevo HTML [sin jsp]:
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Resultado: " + resultado + "</h1>");
+        out.println("<p>Espero lo disfrutes.... felicidades: <p>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
